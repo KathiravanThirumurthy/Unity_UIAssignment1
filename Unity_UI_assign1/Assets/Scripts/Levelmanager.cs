d18a84758a69ef98018d6041f8ad346d47fb0e00
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2022-02-17 13:42:59 +0530

    Back button flow is completed

---
+++ Unity_UI_assign1/Assets/Scripts/Levelmanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Levelmanager : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] _levelBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<_levelBtn.Length;i++)
+        {
+            _levelBtn[i].onClick.AddListener(btnClicked);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void btnClicked()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        string str = EventSystem.current.currentSelectedGameObject.name;
+        SceneManager.LoadScene(str);
+    }
+}
